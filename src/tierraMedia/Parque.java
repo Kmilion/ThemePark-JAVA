68617307888b620b836933b09665f069bf0aa82f
Gonzalo <gonzaloeceballos@gmail.com>
2021-09-15 21:01:49 -0300

    Modificaciones

--- src/tierraMedia/Parque.java
+++ src/tierraMedia/Parque.java
@@ -1,5 +1,6 @@
 package tierraMedia;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,8 +16,11 @@ public class Parque {
 	private String nombreParque;
 
 	/* Constructor del Parque, inicializa el nombre del mismo. */
-	public Parque(String nombre) {
+	public Parque(String nombre, String archivoUsuarios, String archivoAtracciones, String promoP, String promoA,
+			String promoAxB) {
 		this.nombreParque = nombre;
+		this.usuariosParque = getUsuarios(archivoUsuarios);
+		this.sugerenciasParque = getSugerencias(archivoAtracciones, promoP, promoA, promoAxB);
 	}
 
 	/* Devuelve el nombre del Parque. */
@@ -36,11 +40,182 @@ public class Parque {
 		}
 	}
 
+	public ArrayList<Usuario> getUsuarios(String archivo) {
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new File(archivo));
+
+			while (sc.hasNext()) {
+
+				String linea = sc.nextLine();
+				String campos[] = linea.split(",");
+
+				String nombre = campos[0];
+				TipoAtraccion tipoPreferido = TipoAtraccion.valueOf(campos[1]);
+				int dineroDisponible = Integer.parseInt(campos[2]);
+				double tiempoDisponible = Double.parseDouble(campos[3]);
+				Usuario usuario = new Usuario(nombre, tipoPreferido, dineroDisponible, tiempoDisponible);
+
+				usuarios.add(usuario);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+
+		return usuarios;
+	}
+
+	private ArrayList<Sugerencia> getSugerencias(String archivoAtracciones, String PromoP, String PromoA,
+			String PromoAxB) {
+		ArrayList<Sugerencia> sugerencias = new ArrayList<Sugerencia>();
+		ArrayList<Atraccion> atracciones = new ArrayList<Atraccion>();
+		ArrayList<Promocion> promocionesPorcentuales = new ArrayList<Promocion>();
+		ArrayList<Promocion> promocionesAbsolutas = new ArrayList<Promocion>();
+		ArrayList<Promocion> promocionesAxB = new ArrayList<Promocion>();
+
+		Scanner sc = null;
+		try {
+			sc = new Scanner(new File(archivoAtracciones));
+
+			while (sc.hasNext()) {
+
+				String linea = sc.nextLine();
+				String campos[] = linea.split(",");
+				ArrayList<String> datos = new ArrayList<String>();
+				for (String s : campos) {
+					datos.add(s);
+				}
+				String nombre = datos.get(0);
+				int costo = Integer.parseInt(datos.get(1));
+				double tiempo = Double.parseDouble(datos.get(2));
+				int cupo = Integer.parseInt(datos.get(3));
+				TipoAtraccion tipo = TipoAtraccion.valueOf(datos.get(4));
+
+				Atraccion atraccion = new Atraccion(nombre, costo, tiempo, cupo, tipo);
+				atracciones.add(atraccion);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		sc.close();
+
+		Scanner scA = null;
+		try {
+			scA = new Scanner(new File(PromoP));
+
+			while (scA.hasNext()) {
+
+				String linea = scA.nextLine();
+				String campos[] = linea.split(",");
+				ArrayList<String> datos = new ArrayList<String>();
+				for (String s : campos) {
+					datos.add(s);
+				}
+				String nombrePromocion = datos.get(0);
+				TipoAtraccion tipoPromocion = TipoAtraccion.valueOf(datos.get(1));
+				Integer porcentajeDeDescuento = Integer.parseInt(datos.get(2));
+				ArrayList<Atraccion> atraccionesPromo = new ArrayList<Atraccion>();
+				for (int i = 3; i < datos.size(); i++) {
+					for (Atraccion s : atracciones) {
+						if (datos.get(i).equals(s.getNombre())) {
+							atraccionesPromo.add(s);
+						}
+					}
+				}
+				Promocion promocion = new PromocionPorcentual(nombrePromocion, tipoPromocion, atraccionesPromo,
+						porcentajeDeDescuento);
+				promocionesPorcentuales.add(promocion);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		scA.close();
+
+		Scanner scB = null;
+		try {
+			scB = new Scanner(new File(PromoA));
+
+			while (scB.hasNext()) {
+
+				String linea = scB.nextLine();
+				String campos[] = linea.split(",");
+				ArrayList<String> datos = new ArrayList<String>();
+				for (String s : campos) {
+					datos.add(s);
+				}
+				String nombrePromocion = datos.get(0);
+				TipoAtraccion tipoPromocion = TipoAtraccion.valueOf(datos.get(1));
+				Integer precio = Integer.parseInt(datos.get(2));
+				ArrayList<Atraccion> atraccionesPromo = new ArrayList<Atraccion>();
+				for (int i = 3; i < datos.size(); i++) {
+					for (Atraccion s : atracciones) {
+						if (datos.get(i).equals(s.getNombre())) {
+							atraccionesPromo.add(s);
+						}
+					}
+				}
+				Promocion promocion = new PromocionAbsoluta(nombrePromocion, tipoPromocion, atraccionesPromo, precio);
+				promocionesAbsolutas.add(promocion);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		scB.close();
+
+		Scanner scC = null;
+		try {
+			scC = new Scanner(new File(PromoAxB));
+
+			while (scC.hasNext()) {
+
+				String linea = scC.nextLine();
+				String campos[] = linea.split(",");
+				ArrayList<String> datos = new ArrayList<String>();
+				for (String s : campos) {
+					datos.add(s);
+				}
+				String nombrePromocion = datos.get(0);
+				TipoAtraccion tipoPromocion = TipoAtraccion.valueOf(datos.get(1));
+				ArrayList<Atraccion> atraccionesPromo = new ArrayList<Atraccion>();
+				for (int i = 2; i < datos.size(); i++) {
+					for (Atraccion s : atracciones) {
+						if (datos.get(i).equals(s.getNombre())) {
+							atraccionesPromo.add(s);
+						}
+					}
+				}
+				Promocion promocion = new PromocionAxB(nombrePromocion, tipoPromocion, atraccionesPromo);
+				promocionesAxB.add(promocion);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		scC.close();
+
+		for (Atraccion a : atracciones) {
+			sugerencias.add(a);
+		}
+		for (Promocion p : promocionesPorcentuales) {
+			sugerencias.add(p);
+		}
+		for (Promocion p : promocionesAbsolutas) {
+			sugerencias.add(p);
+		}
+		for (Promocion p : promocionesAxB) {
+			sugerencias.add(p);
+		}
+
+		return sugerencias;
+
+	}
+
 	/*
 	 * Si la Sugerencia es una Atraccion devuelve true si se encuentra dentro de las
 	 * Atraccion compradas por el Usuario.
 	 */
-	public static boolean contieneAtraccion1(Usuario u, Sugerencia s) {
+	public boolean contieneAtraccion1(Usuario u, Sugerencia s) {
 		boolean valor = false;
 		if (u.getAtraccionesCompradas().contains(s))
 			valor = true;
@@ -52,7 +227,7 @@ public class Parque {
 	 * por el Usuario y por cada Atraccion comprada pregunta si está contenida
 	 * dentro del ArrayList de las Promocion sugeridas.
 	 */
-	public static boolean contieneAtraccion2(Usuario u, Sugerencia s) {
+	public boolean contieneAtraccion2(Usuario u, Sugerencia s) {
 		boolean valor = false;
 		for (Atraccion a : u.getAtraccionesCompradas()) {
 			if (s.getAtraccionesPromocion().contains(a))
@@ -62,7 +237,7 @@ public class Parque {
 	}
 
 	/* Ordena el ArrayList de Sugerencia pasado por parámetro. */
-	private static void OrdenarAtracciones(ArrayList<Sugerencia> sugerencias) {
+	private void OrdenarAtracciones(ArrayList<Sugerencia> sugerencias) {
 		Collections.sort(sugerencias, new Ordenador());
 	}
 
@@ -126,7 +301,7 @@ public class Parque {
 		System.out.print("******** Fin del servicio ********");
 	}
 
-	public static void escribirUsuario(Usuario u) throws FileNotFoundException {
+	public void escribirUsuario(Usuario u) throws FileNotFoundException {
 		PrintWriter salida = new PrintWriter(u.getNombre() + ".txt");
 
 		salida.println("*******Resumen de compra*******\n");
@@ -148,7 +323,7 @@ public class Parque {
 		salida.close();
 	}
 
-	public static void escribirAtracciones(ArrayList<Sugerencia> sugerencias) throws FileNotFoundException {
+	public void escribirAtracciones(ArrayList<Sugerencia> sugerencias) throws FileNotFoundException {
 		PrintWriter salida = new PrintWriter("resumenAtracciones.txt");
 
 		salida.println("*******Resumen de las atracciones del día*******\n");
